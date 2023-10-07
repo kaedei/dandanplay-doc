ece24555344913a25708557dbba1b879a69642c2
kaedei <kaedei@qq.com>
2023-10-07 23:04:56 +0800

    init project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+    lang: 'zh-CN',
+    title: 'Hello World!',
+    description: '这是弹弹play帮助文档',
+})
---
+++ docs/index.md
@@ -0,0 +1,5 @@
+# 弹弹play帮助文档
+
+使用 VuePress 构建
+
+![弹弹play Logo](/images/logo.png)
